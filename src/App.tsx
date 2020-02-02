316bb969473e1729cac7bca633e31fe94ea2bf52
isoken26 <kisomura26@gmail.com>
2020-02-02 18:47:01 +0900

    create Search and WeatherList Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Search from './components/Search/Search'
+import WeatherList from './components/Wether/WeatherList'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <Search />
+      <WeatherList />>
     </div>
   );
 }
